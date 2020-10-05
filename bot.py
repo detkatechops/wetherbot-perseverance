25e5396e10e785836a72cde71f1933c839d78124
root <truerudeboyltd@gmail.com>
2020-10-05 18:39:04 +0300

    v1.4.2 google translate refactor

--- bot.py
+++ bot.py
@@ -1,27 +1,31 @@
-import config
 import telebot
 import pytaf
 from urllib import request
 from googletrans import Translator
+from config import TOKEN, URL_METAR, URL_TAF
 
-TOKEN = config.TOKEN
-URL_METAR = config.URL_METAR
-URL_TAF = config.URL_TAF
 
+# Создание бота
 bot = telebot.TeleBot(token=TOKEN)
+# создать объект класса Translator
 translator = Translator()
 keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
 keyboard.row('/start', '/get_metar', '/get_taf')
 
+
 def parse_data(code):
+    # заменяте таф таф на таф один раз, один раз режет строку на \n и берет второй элемент по счету, бо первый это 0
     code = code.replace('TAF TAF', 'TAF', 1).split('\n', 1)[1]
+    # в обьект перевода подается обьект конвертирования кода сводки аэропорта взятый из переменной code
     return translator.translate(pytaf.Decoder(pytaf.TAF(code)).decode_taf(), src='en', dest='ru')
+
 
 @bot.message_handler(commands=['start', 'help'])
 def start(message):
     msg = "Привет. Это бот для получения авиационного прогноза погоды " \
           "с серверов NOAA. Бот настроен на аэропорт Жуляны (UKKK)."
     bot.send_message(message.chat.id, msg, reply_markup=keyboard)
+
 
 @bot.message_handler(commands=['get_metar'])
 def get_metar(message):
@@ -30,6 +34,7 @@ def get_metar(message):
     # Send formatted answer.
     bot.send_message(message.chat.id, parse_data(code), reply_markup=keyboard)
 
+
 @bot.message_handler(commands=['get_taf'])
 def get_taf(message):
     # Fetch info from server.
@@ -37,4 +42,7 @@ def get_taf(message):
     # Send formatted answer.
     bot.send_message(message.chat.id, parse_data(code), reply_markup=keyboard)
 
-bot.polling(none_stop=True)
+
+# Запускаем бота, чтобы работал 24/7
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
